5c76f7341eff0560257ed06e0cf7370ec1a10019
YipengChen <chenypmail@163.com>
2018-11-25 15:22:07 +0800

    v0.1

---
+++ hacnn_market1501_train.sh
@@ -0,0 +1,17 @@
+python train_imgreid_xent.py \
+--root data \
+-s market1501 \
+-t market1501 \
+-j 4 \
+--height 160 \
+--width 64 \
+--optim sgd \
+--lr 0.03 \
+--label-smooth \
+--max-epoch 300 \
+--stepsize 150 225 \
+--train-batch-size 32 \
+--test-batch-size 100 \
+-a hacnn \
+--save-dir log/hacnn-market-xent \
+--gpu-devices 0 \
